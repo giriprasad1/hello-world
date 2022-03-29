6516c9313ae0d347a0968e1d1774cc5ae5155644
giriprasad <giriprasad@gmail.com>
2022-03-29 17:01:09 +0530

    Add host and ip information

--- src/main/java/tech/gcplearnng/java/helloworld/resource/HelloWorldResource.java
+++ src/main/java/tech/gcplearnng/java/helloworld/resource/HelloWorldResource.java
@@ -1,5 +1,8 @@
 package tech.gcplearnng.java.helloworld.resource;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldResource {
 
 	@GetMapping("/")
-	public String hello() {
-		return "Hello GCP Learnng";
+	public String hello() throws UnknownHostException {
+		return "Hello GCP Learnng from :" + Inet4Address.getLocalHost().getHostAddress() + " host:" + Inet4Address.getLocalHost().getHostName();
 	}
 }
